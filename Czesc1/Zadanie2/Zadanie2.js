72db180d94d49ba3f07be2f2cb8561e6a03dee63
MarcinJar <marcin.jarowski@oke.pl>
2024-06-04 14:05:00 +0200

    Zadanie2 - rozwiazanie

--- Czesc1/Zadanie2/Zadanie2.js
+++ Czesc1/Zadanie2/Zadanie2.js
@@ -1,4 +1,14 @@
 import fs from "fs";
 import { readData } from "../Zadanie1/Zadanie1.js";
 
+const createFiles = () => {
+  const data = readData();
+  data.forEach((el) => {
+    fs.writeFileSync(
+      `${el.Miasto}-${el.Imie}-${el.Nazwisko}.json`,
+      JSON.stringify(el)
+    );
+  });
+};
 
+createFiles();
