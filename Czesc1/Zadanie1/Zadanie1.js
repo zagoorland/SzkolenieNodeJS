35df1916302abddc607bccebc1d20a922a4a85dc
MarcinJar <marcin.jarowski@oke.pl>
2024-06-04 13:44:42 +0200

    Zadanie1- zmiany

--- Czesc1/Zadanie1/Zadanie1.js
+++ Czesc1/Zadanie1/Zadanie1.js
@@ -2,13 +2,13 @@ import fs from "fs";
 
 export const readData = () => {
   try {
-    const data = fs.readFileSync("dane.json");
+    const data = fs.readFileSync("../dane.json");
     const dataParsed = JSON.parse(data);
+    console.log(dataParsed);
     return dataParsed;
   } catch (err) {
     console.error(err);
   }
 };
 
-const dane = readData();
-console.log(dane);
+readData();
