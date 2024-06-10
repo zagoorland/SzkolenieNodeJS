898dfe75057f54e989d9a5ee6e4b8904a1172943
MarcinJar <marcin.jarowski@oke.pl>
2024-06-10 09:02:14 +0200

    czesc2 - wstep

--- Czesc2/index.js
+++ Czesc2/index.js
@@ -1,1 +1,32 @@
 import express from "express";
+
+const app = express();
+const methodLogger = (req, res, next) => {
+  console.log(req.method);
+  next();
+};
+
+// Middleware globalnie obsługujące błędy
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Coś nie pykło!");
+};
+app.use(errorHandler);
+// Przykładowa ścieżka, która wyrzuca błąd
+app.get("/", (req, res) => {
+  throw new Error("AŁA");
+});
+
+app.use(methodLogger);
+app.get("/", function (req, res) {
+  res.send("Hello world!");
+});
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000");
+});
+
+app.use("/user/:id", function (req, res, next) {
+  console.log(req.method);
+  next();
+});
