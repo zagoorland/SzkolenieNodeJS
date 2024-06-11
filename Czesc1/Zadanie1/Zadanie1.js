815b3c8f4509479d42cdb6d42d77fcd919965df0
Jakub Zagórski <jakub.zagorski@wearetriple.com>
2024-06-11 17:29:44 +0200

    feat: add async file reading function
    This commit adds an async function called `readFile` that reads the contents of a file called "dane.json" using the `fs` module. The file is parsed as JSON and the parsed data is logged to the console. If an error occurs during the reading or parsing process, the error is also logged.

--- Czesc1/Zadanie1/Zadanie1.js
+++ Czesc1/Zadanie1/Zadanie1.js
@@ -1,1 +1,14 @@
 import fs from "fs";
+
+const readFile = async () => {
+  try {
+    const data = fs.readFileSync("../dane.json");
+    const dataParsed = JSON.parse(data);
+
+    console.log(dataParsed);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+readFile();
